ba9cb9766781ddbb213e7e435a8140513f7732df
Mo Nguyen <mo.nguyen@difisoft.com>
2022-04-11 17:31:11 +0700

    fix adding job in example

--- examples/main.go
+++ examples/main.go
@@ -1,60 +1,76 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strconv"
 
-	"github.com/kanguki/scheduler"
+	leaderelection "github.com/kanguki/leader-election"
+	"github.com/kanguki/log"
+	sc "github.com/kanguki/scheduler"
 	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	var scheduler = &scheduler.Driver{
+	flag.Parse()
+	var scheduler = &sc.Driver{
 		Jobs: loadJobs(),
 	}
-	go scheduler.Run()
+	clusterSize, err := strconv.Atoi(os.Getenv(sc.CLUSTER_SIZE))
+	if err != nil {
+		log.Log("Error parsing %v", sc.CLUSTER_SIZE)
+		os.Exit(1)
+	}
+	leBase := leaderelection.Base(os.Getenv(sc.LE_BASE))
+	opts := sc.Opts{LeOpts: leaderelection.LeOpts{Base: leBase, Name: "test", Size: clusterSize, TimeoutDecideLeader: 10}}
+	go scheduler.Run(opts)
 	select {}
 }
 
-func loadJobs() map[string]*scheduler.Job {
-	jobs := map[string]*scheduler.Job{
+func loadJobs() map[string]*sc.Job {
+	jobs := map[string]*sc.Job{
 		"say_lala_every_3_secs": {
 			//from hour 1->19, from sec 0-50, run job every 3 seconds
 			CronTime: "0-50/3 1-19,20-41,42-59 0-19 * * SUN-SAT",
 			Do: func() {
-				scheduler.Log("lala every 3 secs")
+				log.Log("lala every 3 secs")
 			},
 		},
 		"say_hehe": {
 			CronTime: "*/6 * * * * *",
 			Do: func() {
-				scheduler.Log("hehe every 6 secs")
+				log.Log("hehe every 6 secs")
 			},
 		},
 	}
+
 	absPath, _ := filepath.Abs("examples/jobs.yml")
 	ymlJobs := ymlToJob(absPath)
-	for _, job := range ymlJobs {
-		scheduler.Log("%+v", job)
-		jobs[job.Name] = &scheduler.Job{
-			CronTime: job.Time,
+	log.Debug("yamlJobs %v", ymlJobs)
+	for _, v := range ymlJobs {
+		msg := v.Msg
+		jobs[v.Name] = &sc.Job{
+			CronTime: v.Time,
 			Do: func() {
-				scheduler.Log(job.Msg)
-			}}
+				log.Log("%v", msg) //if we directly use v.Msg instead of setting msg and pass here, it will use msg of last item. maybe because goroutine effect here upon array :D
+			},
+		}
 	}
-	scheduler.Log("jobs: %+v", jobs)
+	log.Log("jobs: %+v", jobs)
 	return jobs
 
 }
 func ymlToJob(path string) []YamlJob {
 	yfile, err := ioutil.ReadFile(path)
 	if err != nil {
-		scheduler.Log(err.Error())
+		log.Log(err.Error())
 	}
 	var data YamlJobs
 	err2 := yaml.Unmarshal(yfile, &data)
 	if err2 != nil {
-		scheduler.Log(err2.Error())
+		log.Log(err2.Error())
 	}
 	return data.Jobs
 }
--- examples/run.sh
+++ examples/run.sh
@@ -1,8 +1,9 @@
 # export SCHEDULER_HTTP_PORT=8000 
 export SCHEDULER_DISABLE_HTTP_HANDLER= 
 export LOG_PATH= 
-export SCHEDULER_ELECTOR=zk 
-export ZOOKEEPER_URLS=127.0.0.1:2181 
+export CLUSTER_SIZE=3 #must be an odd number
+export LE_BASE="NATS"
+# export NATS_QUORUM=
 
 rm -f log/* &&
-go run examples/main.go
+go run examples/main.go "$@"
